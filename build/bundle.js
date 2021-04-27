63df35da656f811a55fd765f6272d0af28912461
Artem <a.gorbachev.work@gmail.com>
2021-04-28 02:11:52 +0300

    node, npm, создание и настройка проекта, настройка webpack config

---
+++ build/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => {
+  // webpackBootstrap
+  var __webpack_exports__ = {};
+  /*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+  const x = 123;
+  /******/
+})();
+//# sourceMappingURL=bundle.js.map
